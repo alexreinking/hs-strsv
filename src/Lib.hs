c7877c12ddc464b31af91cd466f4d5ecb6597f7b
Alex Reinking <alex.reinking@gmail.com>
2020-03-20 02:27:57 -0700

    basic working version

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
 module Lib
     ( someFunc
+    , dotp
+    , solve
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+dotp :: Num a => [a] -> [a] -> a
+dotp x y = sum $ zipWith (*) x y
+
+solve' :: Fractional a => [[a]] -> [a] -> [a]
+solve' [] [] = []
+solve' ((l:ls):_L) (b:_b) =
+  let prev = solve' _L _b
+   in (b - dotp ls prev) / l : prev
+
+solve :: Fractional a => [[a]] -> [a] -> [a]
+solve _L b = reverse $ solve' _L' b'
+  where _L' = reverse (map reverse _L)
+        b' = reverse b
